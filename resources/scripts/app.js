f42cf40886d5acef4d5b351c1910f4509261c5df
giantpaper <giantpaper@outlook.com>
2022-05-30 18:13:28 -0700

    Stole sage 9's routing js thing

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -3,11 +3,27 @@ import {domReady} from '@roots/sage/client';
 /**
  * app.main
  */
+import Router from './util/Router';
+import common from './routes/common';
+
 const main = async (err) => {
   if (err) {
     // handle hmr errors
     console.error(err);
   }
+
+// import local dependencies
+
+	/** Populate Router instance with DOM routes */
+	const routes = new Router({
+		// All pages
+		common,
+		// Home page
+		home,
+	});
+
+	// Load Events
+	jQuery(document).ready(() => routes.loadEvents());
 
   // application code
 };
