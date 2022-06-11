ff468bc95f4650a8b459e03a5e20b67e0145fd5b
giantpaper <giantpaper@outlook.com>
2022-06-11 00:16:16 -0700

    Edited app.js

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,38 +1,54 @@
+import jQuery from 'jquery';
 import jqueryUI from './vendor/jquery-ui';
+
+// import getWrunner from './load/wrunner-jquery';
+
 import {domReady} from '@roots/sage/client';
 
 /**
  * app.main
  */
 import Router from './util/Router';
+//
 import common from './routes/common';
+import home from './routes/home';
+// import showGraph from './routes/showGraph';
 
-const main = async (err) => {
-  if (err) {
-    // handle hmr errors
-    console.error(err);
-  }
+window.is_touch_enabled = () => {
+	return ( 'ontouchstart' in window ) ||
+				 ( navigator.maxTouchPoints > 0 ) ||
+				 ( navigator.msMaxTouchPoints > 0 );
+}
 
-// import local dependencies
+(function($){
+	const main = async (err) => {
+		if (err) {
+			// handle hmr errors
+			console.error(err);
+		}
 
-	/** Populate Router instance with DOM routes */
-	const routes = new Router({
-		// All pages
-		common,
-		// Home page
-		home,
-	});
+	// import local dependencies
+		/** Populate Router instance with DOM routes */
+		const routes = new Router({
+			// All pages
+			common,
+			// Home page
+			home,
+			// showGraph,
+		});
 
-	// Load Events
-	jQuery(document).ready(() => routes.loadEvents());
+		// Load Events
+		jQuery(document).ready(() => routes.loadEvents());
 
-  // application code
-};
+		// application code
+	};
 
-/**
- * Initialize
- *
- * @see https://webpack.js.org/api/hot-module-replacement
- */
-domReady(main);
-import.meta.webpackHot?.accept(main);
+	/**
+	 * Initialize
+	 *
+	 * @see https://webpack.js.org/api/hot-module-replacement
+	 */
+	domReady(main);
+	import.meta.webpackHot?.accept(main);
+
+})(jQuery);
